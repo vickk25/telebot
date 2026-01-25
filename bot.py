c29ea77bf20cb8247b8f5b025f3f9c80a4d230c8
vickk25 <victoriakjq@gmail.com>
2026-01-25 08:14:14 +0000

    est

--- bot.py
+++ bot.py
@@ -182,8 +182,6 @@ async def rps_play(update: Update, context: ContextTypes.DEFAULT_TYPE):
 @app.route('/', methods=['GET', 'POST'])
 async def webhook():
     """Handle incoming Telegram updates"""
-
-    telebot.add_handler(CommandHandler("start", start))
     if flask.request.method == "POST":
         await telebot.initialize()
         update = Update.de_json(flask.request.get_json(force=True), telebot.bot)
@@ -199,26 +197,18 @@ async def setup_webhook():
         await telebot.bot.set_webhook(url=WEBHOOK_URL)
         print(f"Webhook set to {WEBHOOK_URL}")
 
+
+
+
 def main() -> None:
     """Start the bot."""
+
     asyncio.run(setup_webhook())
-    # Create the Application and pass it your bot's token.
-
-    # on different commands - answer in Telegram
-
-    # application.add_handler(CallbackQueryHandler(button_callback))
-    # application.add_handler(CommandHandler("help", help_command))
+    telebot.add_handler(CommandHandler("start", start))
     telebot.add_handler(CommandHandler("cat", cat))
     telebot.add_handler(CommandHandler("joke", joke))
     telebot.add_handler(CallbackQueryHandler(joke_callback_handler, pattern="^joke_"))
     telebot.add_handler(CommandHandler("rps", rps_start))
     telebot.add_handler(CallbackQueryHandler(rps_play, pattern="^rps_"))
 
-    # on non command i.e message - echo the message on Telegram
-    telebot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
-    # telebot.run_polling(allowed_updates=Update.ALL_TYPES)
-
-if __name__ == "__main__":
-    main()
+main()
